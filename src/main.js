8a886ff5a911ff873706e52e69ce480e28b8a044
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-23 11:29:16 -0700

    WIP: add UI logic for currency output. Output displays undefined.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,25 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import Example from "./example.js";
-import AnotherExample from "./another-example.js";
+import Currency from "./js/currency.js";
 
+function clearFields() {
+  $("#currencyInput").val("");
+  $("#showCurrency").text(" ");
+}
+
+$(document).ready(function () {
+  $("#dollarsButton").click(event => {
+    event.preventDefault();
+    let currencyAmount = $("#currencyInput").val();
+    clearFields();
+    let promise = Currency.getExchange(currencyAmount);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $("#showCurrency").text(`${body.conversion_rates}`)
+    })
+  })
+})
 
 // function displayExample(photoArray) {
 //   let photoHTML = ``;
@@ -19,16 +35,16 @@ import AnotherExample from "./another-example.js";
 //   }
 // }
 
-$(document).ready(function () {
-  let promise = Example.promiseExample();
-  promise.then(function (response) {
-    const body = JSON.parse(response);
-    $(".apod").html(`<img src=${body.url}>`);
-    $(".explanation").text(`${body.explanation}`);
-  });
-  AnotherExample.fetchExample().then(function (response) {
-    const url = response.collection.items[2].href.replaceAll(" ", "%20");
-    console.log(url);
-    $(".video").html(`<video controls> <source src=${url} type="video/mp4"></video>`);
-  });
-});
+// $(document).ready(function () {
+//   let promise = Example.promiseExample();
+//   promise.then(function (response) {
+//     const body = JSON.parse(response);
+//     $(".apod").html(`<img src=${body.url}>`);
+//     $(".explanation").text(`${body.explanation}`);
+//   });
+//   AnotherExample.fetchExample().then(function (response) {
+//     const url = response.collection.items[2].href.replaceAll(" ", "%20");
+//     console.log(url);
+//     $(".video").html(`<video controls> <source src=${url} type="video/mp4"></video>`);
+//   });
+// });
